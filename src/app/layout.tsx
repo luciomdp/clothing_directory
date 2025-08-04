c41a6cc56eb6467c659ab2ecb1f65e2d96a8b97b
Lucio <luciopaulino@advenio.com.ar>
2025-08-04 09:30:23 -0300

    Feat [layout]: <Quita GA>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
     icon: '/favicon.ico',
   },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,26 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-Y4RR734BS1"
-          strategy="afterInteractive"
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-Y4RR734BS1');
-            `,
-          }}
-        />
         {children}
       </body>
     </html>
